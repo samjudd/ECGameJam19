ac3888f044486307e3060313bc0c53aa6c0742f3
Benji Lin <benjaleelin@gmail.com>
2019-03-02 12:05:25 -0800

    start menu with simple space to start

---
+++ menu.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class menu : MarginContainer
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+
+    public override void _Process(float delta)
+    {
+        // Called every frame. Delta is time since last frame.
+        // Update game logic here.
+		if (Input.IsActionPressed("start_game"))
+        {
+			GetTree().ChangeScene("res://main.tscn");
+        }
+        
+    }
+}
