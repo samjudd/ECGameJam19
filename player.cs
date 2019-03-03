f90fdac9d8b2c999a8a95fcf37f0aa4353692e51
Benji Lin <benjaleelin@gmail.com>
2019-03-03 00:37:38 -0800

    forgot equals

--- player.cs
+++ player.cs
@@ -108,7 +108,7 @@ public class player : KinematicBody2D
                 }
                 break;
             case state.BOOTS_ATTACK:
-                if (_attack_time < 0.5)
+                if (_attack_time <= 0.5)
                 {
                     _velocity = this.GetRotateChild().Transform.y * _speed * 10.0f;
                     _attack_time += delta;
